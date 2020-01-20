53d9650411d62ec55ee2090cda786c60b752e4e9
Nissykh1127 <59646773+Nissykh1127@users.noreply.github.com>
2020-01-20 14:17:19 +0900

    非同期通信

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.action_view.automatically_disable_submit_tag = false
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
