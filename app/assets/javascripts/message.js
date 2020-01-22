4b700b1aa53f706af8ea633a371584ad900dc51e
Nissykh1127 <59646773+Nissykh1127@users.noreply.github.com>
2020-01-22 13:09:37 +0900

    自動更新機能追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,55 @@
 $(function(){
-  function buildHTML(message){
-    if ( message.image ) {
-      var html =  
-      `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="view-name">
-              ${message.user_name}
-            </div>
-            <div class="view-data">
-              ${message.created_at}
-            </div>
-          </div>
-        <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-            <img src=${message.image}>
-          </div>
-        </div>`
-      return html;
-    } else {
-      var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="view-name">
-              ${message.user_name}
-            </div>
-            <div class="view-data">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
-    };
-  }
-
+var buildHTML = function(message) {
+  if (message.content && message.image) {
+    var html = `<div class="message" data-message-id=` + message.id + `>` +
+      `<div class="upper-message">` +
+        `<div class="view-name">` +
+          message.user_name +
+        `</div>` +
+        `<div class="view-data">` +
+          message.created_at +
+        `</div>` +
+      `</div>` +
+      `<div class="lower-message">` +
+        `<p class="lower-message__content">` +
+          message.content +
+        `</p>` +
+        `<img src="` + message.image + `" class="lower-message__image" >` +
+      `</div>` +
+    `</div>`
+  } else if (message.content) {
+    var html = `<div class="message" data-message-id=` + message.id + `>` +
+      `<div class="upper-message">` +
+        `<div class="view-name">` +
+          message.user_name +
+        `</div>` +
+        `<div class="view-data">` +
+          message.created_at +
+        `</div>` +
+      `</div>` +
+      `<div class="lower-message">` +
+        `<p class="lower-message__content">` +
+          message.content +
+        `</p>` +
+      `</div>` +
+    `</div>`
+  } else if (message.image) {
+    var html = `<div class="message" data-message-id=` + message.id + `>` +
+      `<div class="upper-message">` +
+        `<div class="view-name">` +
+          message.user_name +
+        `</div>` +
+        `<div class="view-data">` +
+          message.created_at +
+        `</div>` +
+      `</div>` +
+      `<div class="lower-message">` +
+        `<img src="` + message.image + `" class="lower-message__image" >` +
+      `</div>` +
+    `</div>`
+  };
+  return html;
+};
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -63,4 +73,31 @@ $(function(){
     　alert("メッセージ送信に失敗しました");
   　})
   });
-}); 
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.chat-main-view').animate({ scrollTop: $('.chat-main-view')[0].scrollHeight});
+      $("form")[0].reset();
+      $(".submit-btn").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      alert('更新に失敗しました');
+    });
+  };
+    // if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    //   setInterval(reloadMessages, 7000);
+  }
+});
